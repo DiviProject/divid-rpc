59742d6d149a10552067b287e1cb952faabf99af
James Encke <james@encke.com>
2021-06-27 19:29:19 -0600

    Fix examples to include JSON formatting

--- examples/promise.js
+++ examples/promise.js
@@ -5,14 +5,27 @@ const CFG = require('./config/')
 const rpc = new RpcClient({
   user: CFG.divi.user,
   pass: CFG.divi.pass,
+  host: CFG.divi.host,
   protocol: 'http'
 })
+
+const formatArgs = argarray => {
+  if (!argarray) {
+    argarray = []
+  }
+  for (let i = 0; i < argarray.length; i++) {
+    if (Array.isArray(argarray[i]) || typeof argarray[i] == 'object') {
+      argarray[i] = JSON.stringify(argarray[i])
+    }
+  }
+  return argarray
+}
 
 const rpcFunction = (functionName, argarray) =>
   new Promise(resolve =>
     rpc[functionName](
       ...[
-        ...(argarray ? argarray : []),
+        ...formatArgs(argarray),
         ...[(err, res) => resolve(err ? { error: err } : res)]
       ]
     )
